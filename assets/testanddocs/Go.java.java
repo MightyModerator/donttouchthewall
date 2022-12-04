586da5f5184df5ce17b656e06a66d2956eb021c1
polymoses <mspam@polymorph.at>
2022-12-04 22:59:17 +0100

    Revert "Maincommit"

--- assets/testanddocs/Go.java.java
+++
@@ -1,6 +0,0 @@
-package io.github.jiangdequan;
-
-import org.apache.logging.log4j.core.config.plugins.util.ResolverUtil.Test;
-
-public @interface Go.java {
-}    
--- src/App.java
+++
@@ -1,9 +0,0 @@
-import at.ddb.teamwork.Game;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Game game = new Game();
-        
-        game.setVisible(true);
-    }
-}
